c0847057f846ec4f492229d46533d6af80be9acc
jalvarezotero <you@example.com>
2018-11-05 11:46:34 +0100

    acabado

---
+++ boletin_6_3/src/boletin_6_3/Boletin_6_3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_6_3;
+
+/**
+ *
+ * @author jalvarezotero
+ */
+public class Boletin_6_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        condicional6_3 obxcon = new condicional6_3();
+        obxcon.Numero();
+    }
+
+}
